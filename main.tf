a0d4a5764c05671be99122324c99d17b1aebd1e1
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-22 16:31:24 +0530

    ss

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_cluster" "eks" {
   name     = "${var.ENV}-eks-cluster"
-  role_arn = aws_iam_role.example.arn
+  role_arn = aws_iam_role.eks-cluster-role.arn
 
   vpc_config {
     subnet_ids = var.PRIVATE_SUBNET_IDS
