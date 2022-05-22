73ff16fcf45c4fb4e791a5d2b151d2b14a1ade90
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-22 18:49:33 +0530

    ss

--- tags.tf
+++ tags.tf
@@ -12,14 +12,14 @@ resource "aws_ec2_tag" "private-subnets-lb" {
   value       = "1"
 }
 
-resource "aws_ec2_tag" "private-subnets" {
+resource "aws_ec2_tag" "public-subnets" {
   count = length(var.PUBLIC_SUBNET_IDS)
   resource_id = element(var.PRIVATE_SUBNET_IDS, count.index)
   key         = "kubernetes.io/cluster/${var.ENV}-eks-cluster"
   value       = "Owned"
 }
 
-resource "aws_ec2_tag" "private-subnets-lb" {
+resource "aws_ec2_tag" "public-subnets-lb" {
   count = length(var.PUBLIC_SUBNET_IDS)
   resource_id = element(var.PRIVATE_SUBNET_IDS, count.index)
   key         = "kubernetes.io/role/elb"
