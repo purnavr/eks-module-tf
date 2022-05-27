45ac4b161647f4454a50632b28b738ae7c0de098
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2022-05-27 07:53:33 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -2,5 +2,5 @@ variable "ENV" {}
 variable "PRIVATE_SUBNET_IDS" {}
 variable "PUBLIC_SUBNET_IDS" {}
 variable "DESIRED_SIZE" {}
-varibale "MAX_SIZE" {}
+variable "MAX_SIZE" {}
 variable "MIN_SIZE" {}
