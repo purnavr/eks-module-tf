36f38db92072396601387f941887b3c526d0cf36
Raghu K <raghuk.vit@gmail.com>
2022-06-22 20:47:02 +0530

    ss

--- kube-config.tf
+++ kube-config.tf
@@ -1,5 +1,13 @@
 resource "null_resource" "get-kube-config" {
+  depends_on = [aws_eks_node_group.node-group]
   provisioner "local-exec" {
     command = "aws eks update-kubeconfig  --name ${var.ENV}-eks-cluster"
   }
 }
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "my-first-namespace"
+  }
+}
+
