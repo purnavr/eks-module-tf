7d5c99189a9ce690ae341790635e388381313635
Raghu K <raghuk.vit@gmail.com>
2022-06-30 22:13:48 +0530

    ss

--- oidc.tf
+++ oidc.tf
@@ -1,3 +1,7 @@
+data "external" "thumb" {
+  program = ["kubergrunt", "eks", "oidc-thumbprint", "--issuer-url", aws_eks_cluster.eks.identity.0.oidc.0.issuer]
+}
+
 resource "aws_iam_openid_connect_provider" "cluster" {
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = []
