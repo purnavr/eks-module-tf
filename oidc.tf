f760d53f59fcebeb9beb173c0e8791cfeb932f02
Raghu K <raghuk.vit@gmail.com>
2022-06-30 22:14:36 +0530

    ss

--- oidc.tf
+++ oidc.tf
@@ -4,7 +4,7 @@ data "external" "thumb" {
 
 resource "aws_iam_openid_connect_provider" "cluster" {
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = []
+  thumbprint_list = [data.external.thumb.result.thumbprint]
   url             = aws_eks_cluster.eks.identity.0.oidc.0.issuer
 }
 
